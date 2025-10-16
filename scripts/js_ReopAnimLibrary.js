3891f7df72dea21a9e0359bb1b68aae7a76ebd6a
RileyTyler12 <coolrileytyler@gmail.com>
2025-10-16 19:08:04 -0400

    Add/Update some anims using css keyframes

--- scripts/js_ReopAnimLibrary.js
+++ scripts/js_ReopAnimLibrary.js
@@ -22,13 +22,23 @@ function testAnim(element) {
 }
 
 //a bounce-like animation where the element goes down then back up
-function bounceAnim(element, extent, speed) {
+function bounceAnim(element) {
+   //add the anim class
+   element.classList.add("bounce-anim");
+
+   //remove the class after the animation finishes
+   element.addEventListener('animationend', () => {
+    element.classList.remove("bounce-anim");
+   });
+}
+
+function bounceAltAnim(element, extent, speed) {
     let pos = 0;
     let reset = false;
     requestAnimationFrame(timerFrame);
     function timerFrame() {
         if (pos <= -1) {
-            element.style.top = '0px';
+            element.style.top = '0em';
         } 
         else {
             if (pos >= extent || reset === true){
@@ -54,7 +64,7 @@ function bounceAnim2(element, extent, speed) {
     requestAnimationFrame(timerFrame);
     function timerFrame() {
         if (pos >= 1) {
-            element.style.top = '0px';
+            element.style.top = '0em';
         } 
         else {
             if (-(pos) >= extent || reset === true){
@@ -81,7 +91,7 @@ function bounceLeftAnim(element, extent, speed) {
     requestAnimationFrame(timerFrame);
     function timerFrame() {
         if (pos <= -1) {
-            element.style.left = '0px';
+            element.style.left = '0em';
         } 
         else {
             if (pos >= extent || reset === true){
@@ -99,4 +109,26 @@ function bounceLeftAnim(element, extent, speed) {
             }
         }
     }
+}
+
+//a bounce-like animation to the right
+function bounceRightAnim(element) {
+    //add the anim class
+    element.classList.add("bounce-right-anim");
+
+    //remove the class after the animation finishes
+    element.addEventListener('animationend', () => {
+    element.classList.remove("bounce-right-anim");
+   });
+}
+
+//a left to right css anim call function
+function leftRightAnim(element) {
+    //add the anim class
+    element.classList.add("left-right-anim");
+
+    //remove the class after the animation finishes
+    element.addEventListener('animationend', () => {
+    element.classList.remove("left-right-anim");
+   });
 }
