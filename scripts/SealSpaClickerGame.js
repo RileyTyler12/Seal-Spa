9920425ecdb7a3ab0a0b73364584d71f9c613d91
RileyTyler12 <coolrileytyler@gmail.com>
2025-10-19 18:14:38 -0400

    Update logo in main content and adjust game version to 0.6.5 Prototype

--- scripts/SealSpaClickerGame.js
+++ scripts/SealSpaClickerGame.js
@@ -28,7 +28,7 @@ class Player {
 }
 
 //FOR NOW, if version of game is different from localStorage or empty, clear local storage, resetting it and setting the new version. (change if this causes problems with other stuff on the site.)
-let gameVersion = "0.6.0 Prototype";
+let gameVersion = "0.6.5 Prototype";
 if (localStorage.getItem('SealSpaVersion') === null || localStorage.getItem('SealSpaVersion') !== gameVersion) {
         localStorage.clear();
         localStorage.setItem("SealSpaVersion", gameVersion);
