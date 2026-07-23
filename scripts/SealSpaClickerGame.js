7be81cf7174967efb217c86dd64e0952b05bcc83
RileyTyler12 <coolrileytyler@gmail.com>
2026-07-23 01:32:14 -0400

    Bump game version to 0.7.7

--- scripts/SealSpaClickerGame.js
+++ scripts/SealSpaClickerGame.js
@@ -276,7 +276,7 @@ class Player {
 }
 
 //FOR NOW, if version of game is different from localStorage or empty, clear local storage, resetting it and setting the new version. (change if this causes problems with other stuff on the site.)
-let gameVersion = "0.7.6";
+let gameVersion = "0.7.7";
 if (localStorage.getItem('SealSpaVersion') === null || localStorage.getItem('SealSpaVersion') !== gameVersion) {
         localStorage.clear();
         localStorage.setItem("SealSpaVersion", gameVersion);
